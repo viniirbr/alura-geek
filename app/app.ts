d331a3fede8cc26315610b569fc5f79893635ac6
Vinicius Ribeiro <vayne99@gmail.com>
2022-03-19 11:03:04 -0300

    Get data from server, then create controller

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,7 @@
 import { ProductsController } from './src/controllers/ProductsController.js'
 import { toggleDisplay } from "./src/scripts/toggleDisplay.js"
 import { inputLabelAnimation } from "./src/scripts/inputLabelAnimation.js";
+import { Product } from './src/models/Product.js';
 
 const MOBILE_MAX_WIDTH: number = 600;
 
@@ -17,12 +18,20 @@ if (window.screen.availWidth < MOBILE_MAX_WIDTH) { //functions to be executed in
     })
 }
 
-//ProductsController.loadProductsByCategory()
-fetch('/products.json').then(response => {
-    return response.json()
-}).then(data => {
-    console.log(data)
-})
+fetch('https://alura-geek.herokuapp.com/products')
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
+        let products: Array<Product> = data;
+        let controller = new ProductsController(products)
+        controller.showProducts()
+        
+    })
+
+    
+
+
 
 const input = document.querySelector('.footer__input-text');
 const label = document.querySelector('.footer__form-name');
--- app/dist/src/app.js
+++ app/dist/src/app.js
@@ -1,3 +1,4 @@
+import { ProductsController } from './src/controllers/ProductsController.js';
 import { toggleDisplay } from "./src/scripts/toggleDisplay.js";
 import { inputLabelAnimation } from "./src/scripts/inputLabelAnimation.js";
 const MOBILE_MAX_WIDTH = 600;
@@ -11,10 +12,14 @@ if (window.screen.availWidth < MOBILE_MAX_WIDTH) {
         toggleDisplay(loginButton);
     });
 }
-fetch('/products.json').then(response => {
-    return response.json();
-}).then(data => {
-    console.log(data);
+fetch('https://alura-geek.herokuapp.com/products')
+    .then((res) => {
+    return res.json();
+})
+    .then((data) => {
+    let products = data;
+    let controller = new ProductsController(products);
+    controller.showProducts();
 });
 const input = document.querySelector('.footer__input-text');
 const label = document.querySelector('.footer__form-name');
