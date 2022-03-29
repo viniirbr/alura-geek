e555e0d25d0143bd33a8daaca7509609ccb4024f
Vinicius Ribeiro <vayne99@gmail.com>
2022-03-29 10:47:53 -0300

    Create createProduct function

--- app/app.ts
+++ app/app.ts
@@ -48,3 +48,4 @@ inputLabelAnimation(label, input);
 
 
 
+
---
+++ app/src/scripts/login.ts
@@ -0,0 +1,31 @@
+import { Product } from "../models/Product";
+
+const submitButton = document.querySelector('[login-form__button]') as HTMLElement;
+
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+})
+
+function createProduct(product:Product) {
+    const randonId = Math.ceil(Math.random()*1000);
+    const object = {
+        "id": randonId,
+        "name": product.name,
+        "imgUrl": product.imgUrl,
+        "price": product.price,
+        "category": product.category,
+        "linkToPage": product.linkToPage
+    }
+    const options = {
+        method:'POST',
+        body: JSON.stringify(object),
+        headers: {
+            'Content-Type':'application/json'
+        }
+    };
+
+    fetch('https://alura-geek.herokuapp.com/products', options)
+    .catch((e) => console.log(e.status));
+}
